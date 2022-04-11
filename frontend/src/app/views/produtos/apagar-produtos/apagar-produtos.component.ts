28ccea2b33703f947157a45d57d1e888dbf58a2f
Wesley Soares <wesleybs90@gmail.com>
2022-04-11 11:25:04 -0300

    Adicionar e integrar componente de cadastro de Produtos

---
+++ frontend/src/app/views/produtos/apagar-produtos/apagar-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apagar-produtos',
+  templateUrl: './apagar-produtos.component.html',
+  styleUrls: ['./apagar-produtos.component.scss']
+})
+export class ApagarProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
