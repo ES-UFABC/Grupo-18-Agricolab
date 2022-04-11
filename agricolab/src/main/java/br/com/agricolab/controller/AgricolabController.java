b39527af8c5f4e741d844f2ceb5f8f3c6c42b61d
Wesley Soares <wesleybs90@gmail.com>
2022-04-11 10:39:35 -0300

    Ajuste no CORS

--- agricolab/src/main/java/br/com/agricolab/controller/AgricolabController.java
+++ agricolab/src/main/java/br/com/agricolab/controller/AgricolabController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/agricolab/v1")
 public class AgricolabController {
