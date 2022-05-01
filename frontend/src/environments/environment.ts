3280f4aa94310c7493464025b6ece8ceb2d57e23
Wesley Soares <wesleybs90@gmail.com>
2022-05-01 19:53:09 -0300

    Ajustes gerais no front

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   // API: 'http://localhost:8080',
   API: 'http://agricolab-env-1.eba-stu3nrzp.us-east-1.elasticbeanstalk.com',
   GEO_API: 'http://api.positionstack.com/v1/forward',
+  GEO_API_MAPTILER: 'https://api.maptiler.com/geocoding',
 };
 
 /*
