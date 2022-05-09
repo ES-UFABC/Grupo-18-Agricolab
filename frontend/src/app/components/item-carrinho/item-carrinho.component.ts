d662de1ccfa90124fb7621899201e05d834e6c7c
Wesley Soares <wesleybs90@gmail.com>
2022-05-09 17:00:35 -0300

    Adicionar componente Item Carrinho

---
+++ frontend/src/app/components/item-carrinho/item-carrinho.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ProdutoPedido } from 'src/app/model/produto-pedido.model';
+
+@Component({
+  selector: 'app-item-carrinho',
+  templateUrl: './item-carrinho.component.html',
+  styleUrls: ['./item-carrinho.component.scss']
+})
+export class ItemCarrinhoComponent implements OnInit {
+  @Input() produto: ProdutoPedido;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  removerProduto() {
+
+  }
+}
