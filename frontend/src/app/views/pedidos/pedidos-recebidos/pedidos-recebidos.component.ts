8498d40c7f7a12a790ee465ef717ef2495992908
Wesley Soares <wesleybs90@gmail.com>
2022-05-10 18:58:59 -0300

    Adicionar componente para Listar Pedidos Recebidos pelo Produtor

---
+++ frontend/src/app/views/pedidos/pedidos-recebidos/pedidos-recebidos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos-recebidos',
+  templateUrl: './pedidos-recebidos.component.html',
+  styleUrls: ['./pedidos-recebidos.component.scss']
+})
+export class PedidosRecebidosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
